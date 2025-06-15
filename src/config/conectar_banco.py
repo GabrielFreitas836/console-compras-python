1c87246c38cd9bdb670272d2d48c11d18988885b
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-06-15 10:38:44 -0300

    Adição de verificação de conexão já existente para evitar reconexões

--- src/config/conectar_banco.py
+++ src/config/conectar_banco.py
@@ -11,12 +11,19 @@ import mysql.connector
 # Classe responsável por fazer a conexão com o banco MySQL
 class ConectarBanco:
     
+    # Função inicializadora que define a conexão do banco nula
+    def __init__(self):
+        self.conn = None
 
     # Função de conexão com o banco de dados
+    # Verificação de conexão fechada ou inexistente para abrir essa conexão
     def conectar_ao_banco(self, bd_config):
-        self.bd_config = bd_config
-        self.conn = mysql.connector.connect(**self.bd_config)
-        print("Banco conectado com sucesso")
+        if self.conn is None or not self.conn.is_connected():
+            self.bd_config = bd_config
+            self.conn = mysql.connector.connect(**self.bd_config)
+            print("Banco conectado com sucesso")
+        else:
+            pass
         return self.conn
     
     # Função que fecha a conexão com o banco de dados
