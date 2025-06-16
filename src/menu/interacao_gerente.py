116448a1e01a6dc78dc58d8d9dc83f05c9fb4de5
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-06-16 15:05:27 -0300

    Adicionado mais opções para cada guia de escolha do gerente
    Pequenas modificações visuais (= *50)

--- src/menu/interacao_gerente.py
+++ src/menu/interacao_gerente.py
@@ -14,4 +14,22 @@ class Gerente(ConectarBanco):
     def __init__(self):
         super().__init__()
 
-    
+    def opcoes_gerente(self):
+        print("\n[1] - Clientes\n[2] - Produtos\n")
+        escolha = int(input("Em qual categoria gostaria de mexer ? "))
+
+        match escolha:
+            case 1:
+                print("Abrindo aba de clientes...")
+                time.sleep(1)
+                print("=" *50)
+                print("\n[1] - Adicionar um novo cliente\n[2] - Alterar dados de um cliente existente\n[3] - Deletar um cliente existente\n")
+                print("=" *50)
+            case 2:
+                print("Abrindo aba de produtos...")
+                time.sleep(1)
+                print("=" *50)
+                print("\n[1] - Adicionar um novo produto\n[2] - Alterar dados de um produto existente\n[3] - Deletar um produto existente\n")
+                print("=" *50)
+            case _:
+                print("Escolha 1 ou 2")
