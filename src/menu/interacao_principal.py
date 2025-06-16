a73d5c288f75c78d41a4ba0daa5f289fc9794863
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-06-16 15:04:01 -0300

    Agora a conexão é encerrada toda vez antes de fechar o loop da aplicação

--- src/menu/interacao_principal.py
+++ src/menu/interacao_principal.py
@@ -21,23 +21,27 @@ class Menu:
         while True:
             self.bd_config = bd_config
             self.cliente.conectar_ao_banco(bd_config)
-            print("=" *30)
+            self.gerente.conectar_ao_banco(bd_config)
+            print("=" *50)
             print("Olá! Seja bem-vindo(a) ao sistema de gerenciamento!")
             print("\n [1] - cliente\n [2] - gerente\n")
             entidade = int(input("Você é cliente ou gerente ? Aperte 1 ou 2 "))
             if entidade == 1:
+                time.sleep(0.2)
                 print("Carregando lista de clientes...")
                 self.cliente.carregar_clientes(bd_config)
                 time.sleep(1)
                 if self.cliente.rows == []:
-                    print("=" *30)
+                    print("=" *50)
                     print("\n [1] - sim\n [2] - não\n")
                     continuarRegistro = self.cliente.registrar_cliente()
                     if not continuarRegistro:
+                        self.cliente.fechar_conexao()
                         break
 
                     continuarEscolha = self.cliente.escolher_cliente()
                     if not continuarEscolha:
+                        self.cliente.fechar_conexao()
                         break
                 else:
                     print(tabulate(self.cliente.rows, headers=self.cliente.columms, tablefmt="grid"))
@@ -45,13 +49,19 @@ class Menu:
                     print("\n [1] - sim\n [2] - não\n")
                     continuarRegistro = self.cliente.registrar_cliente()
                     if not continuarRegistro:
+                        self.cliente.fechar_conexao()
                         break
 
                     continuarEscolha = self.cliente.escolher_cliente()
                     if not continuarEscolha:
+                        self.cliente.fechar_conexao()
                         break
             elif entidade == 2:
+                time.sleep(0.2)
+                print("=" *50)
                 print("Bem-vindo, gerente!")
+                self.gerente.opcoes_gerente()
+                self.gerente.fechar_conexao()
                 break
             else:
                 print("Por favor, digite [1] se for cliente ou [2] se for gerente")
