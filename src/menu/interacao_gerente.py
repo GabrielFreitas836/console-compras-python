b58fedbc90578af6de53ae28a8be143aa877b164
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-06-17 14:28:45 -0300

    Adicionado a inserção de novos clientes por parte do gerente e remoção de clientes já existentes no banco

--- src/menu/interacao_gerente.py
+++ src/menu/interacao_gerente.py
@@ -26,7 +26,36 @@ class Gerente(ConectarBanco):
                 time.sleep(1)
                 print("=" *50)
                 print("\n[1] - Adicionar um novo cliente\n[2] - Alterar dados de um cliente existente\n[3] - Deletar um cliente existente\n")
+                subescolha = int(input("O que você gostaria de fazer ? "))
                 print("=" *50)
+
+                if subescolha == 1:
+                    nome = input("Digite o nome do cliente: ")
+                    idade = int(input("Digite a idade do cliente: "))
+                    cursor = self.conn.cursor()
+                    cursor.execute("INSERT INTO clientes (nome, idade) VALUES (%s, %s)", (nome, idade,))
+                    self.conn.commit()
+                    time.sleep(1)
+                    print(f"Cliente {nome} adicionado com sucesso!")
+                elif subescolha == 2:
+                    print("Update em breve...")
+                elif subescolha == 3:
+                    time.sleep(1)
+                    print("\n")
+                    cursor = self.conn.cursor()
+                    cursor.execute("SELECT idCliente, nome FROM clientes")
+                    self.columms = [desc[0] for desc in cursor.description]
+                    self.rows = cursor.fetchall()
+                    print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
+                    time.sleep(1)
+                    print("\n")
+                    clienteDeletado = int(input("Escolha na lista de clientes quem você quer deletar pelo ID: "))
+                    for id, nome in self.rows:
+                        if clienteDeletado == id:
+                            cursor.execute("DELETE FROM clientes WHERE idCliente = %s", (clienteDeletado,))
+                            self.conn.commit()
+                            print(f"Cliente {nome} deletado com sucesso")
+                    
             case 2:
                 print("Abrindo aba de produtos...")
                 time.sleep(1)
