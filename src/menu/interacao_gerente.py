bad2536ac75b5fb7889b6441c09f98786d42d40f
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-06-26 18:20:05 -0300

    Ajustes da posição de try/except, adição de else condition e correções de erros

--- src/menu/interacao_gerente.py
+++ src/menu/interacao_gerente.py
@@ -159,114 +159,113 @@ class Gerente(ConectarBanco):
 
     # Função de atualização dos dados de um cliente
     def atualizar_cliente(self):
-        time.sleep(0.7)
-        print("\n[1] - Alterar somente nome do cliente\n[2] - Alterar somente idade do cliente\n[3] - Alterar todos os dados do cliente\n")
-        escolha = int(input("Escolha uma das opções acima: "))
-        if escolha == 1:
-            time.sleep(0.7)
-            cursor = self.conn.cursor()
-            cursor.execute("SELECT idCliente, nome FROM clientes;")
-            self.columms = [desc[0] for desc in cursor.description]
-            self.rows = cursor.fetchall()
-            print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
-            print("\n")
-            time.sleep(0.7)
+        try:
             while True:
-                try:
+                time.sleep(0.7)
+                print("\n[1] - Alterar somente nome do cliente\n[2] - Alterar somente idade do cliente\n[3] - Alterar todos os dados do cliente\n")
+                escolha = int(input("Escolha uma das opções acima: "))
+    
+                if escolha == 1:
+                    time.sleep(0.7)
+                    cursor = self.conn.cursor()
+                    cursor.execute("SELECT idCliente, nome FROM clientes;")
+                    self.columms = [desc[0] for desc in cursor.description]
+                    self.rows = cursor.fetchall()
+                    print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
+                    print("\n")
+                    time.sleep(0.7)
+
                     idCliente = int(input("Escolha na lista de clientes qual nome você quer alterar pelo ID: "))
                     novoNome = input("Escolha um novo nome: ")
-                except ValueError:
-                    print("Por favor, digite um número válido!")
-                    continue
 
-                for id, nome in self.rows:
-                    if idCliente == id:
-                        cursor = self.conn.cursor()
-                        cursor.execute("UPDATE clientes SET nome = %s WHERE idCliente = %s;", (novoNome, idCliente, ))
-                        self.conn.commit()
-                        nome = novoNome
-                        print("Cliente teve seu nome alterado com sucesso!")
+                    for id, nome in self.rows:
+                        if idCliente == id:
+                            cursor = self.conn.cursor()
+                            cursor.execute("UPDATE clientes SET nome = %s WHERE idCliente = %s;", (novoNome, idCliente, ))
+                            self.conn.commit()
+                            nome = novoNome
+                            print("Cliente teve seu nome alterado com sucesso!")
+                            break
+
+                    if nome != novoNome:
+                        print("Cliente não encontrado! Tente novamente!")
+                        continue
+                    else:
                         break
 
-                if nome != novoNome:
-                    print("Cliente não encontrado! Tente novamente!")
-                    continue
-                else:
-                    break
+                elif escolha == 2:
+                    time.sleep(0.7)
+                    cursor = self.conn.cursor()
+                    cursor.execute("SELECT idCliente, nome, idade FROM clientes;")
+                    self.columms = [desc[0] for desc in cursor.description]
+                    self.rows = cursor.fetchall()
+                    print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
+                    print("\n")
+                    time.sleep(0.7)
 
-        elif escolha == 2:
-            time.sleep(0.7)
-            cursor = self.conn.cursor()
-            cursor.execute("SELECT idCliente, nome, idade FROM clientes;")
-            self.columms = [desc[0] for desc in cursor.description]
-            self.rows = cursor.fetchall()
-            print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
-            print("\n")
-            time.sleep(0.7)
-            while True:
-                try:
                     idCliente = int(input("Escolha na lista de clientes qual idade você quer alterar pelo ID: "))
                     novaIdade = int(input("Escolha uma nova idade: "))
-                except ValueError:
-                    print("Por favor, digite um número válido!")
-                    continue
 
-                for id, nome, idade in self.rows:
-                    if id == idCliente:
-                        cursor = self.conn.cursor()
-                        cursor.execute("UPDATE clientes SET idade = %s WHERE idCliente = %s;", (novaIdade, idCliente, ))
-                        self.conn.commit()
-                        idade = novaIdade
-                        print("Cliente teve sua idade alterada com sucesso!")
+                    for id, nome, idade in self.rows:
+                        if id == idCliente:
+                            cursor = self.conn.cursor()
+                            cursor.execute("UPDATE clientes SET idade = %s WHERE idCliente = %s;", (novaIdade, idCliente, ))
+                            self.conn.commit()
+                            idade = novaIdade
+                            print("Cliente teve sua idade alterada com sucesso!")
+                            break
+
+                    if idade != novaIdade:
+                        print("Cliente não encontrado! Tente novamente!")
+                        continue
+                    else:
                         break
 
-                if idade != novaIdade:
-                    print("Cliente não encontrado! Tente novamente!")
-                    continue
-                else:
-                    break
+                elif escolha == 3:
+                    time.sleep(0.7)
+                    cursor = self.conn.cursor()
+                    cursor.execute("SELECT idCliente, nome, idade FROM clientes;")
+                    self.columms = [desc[0] for desc in cursor.description]
+                    self.rows = cursor.fetchall()
+                    print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
+                    print("\n")
+                    time.sleep(0.7)
 
-        elif escolha == 3:
-            time.sleep(0.7)
-            cursor = self.conn.cursor()
-            cursor.execute("SELECT idCliente, nome, idade FROM clientes;")
-            self.columms = [desc[0] for desc in cursor.description]
-            self.rows = cursor.fetchall()
-            print(tabulate(self.rows, headers=self.columms, tablefmt="grid"))
-            print("\n")
-            time.sleep(0.7)
-            while True:
-                try:
                     idCliente = int(input("Escolha na lista de clientes qual nome e idade você quer alterar pelo ID: "))
                     novoNome = input("Escolha um novo nome: ")
                     novaIdade = int(input("Escolha uma nova idade: "))
-                except ValueError:
-                    print("Por favor, digite um número válido!")
-                    continue
 
-                for id, nome, idade in self.rows:
-                    if id == idCliente:
-                        cursor = self.conn.cursor()
-                        cursor.execute("UPDATE clientes SET nome = %s, idade = %s WHERE idCliente = %s;", (novoNome, novaIdade, idCliente, ))
-                        self.conn.commit()
-                        nome = novoNome
-                        idade = novaIdade
-                        print("Cliente teve seu nome e idade alterados com sucesso!")
+                    for id, nome, idade in self.rows:
+                        if id == idCliente:
+                            cursor = self.conn.cursor()
+                            cursor.execute("UPDATE clientes SET nome = %s, idade = %s WHERE idCliente = %s;", (novoNome, novaIdade, idCliente, ))
+                            self.conn.commit()
+                            nome = novoNome
+                            idade = novaIdade
+                            print("Cliente teve seu nome e idade alterados com sucesso!")
+                            break
+                        
+                    if nome != novoNome and idade != novaIdade:
+                        print("Cliente não encontrado! Tente novamente!")
+                        continue
+                    else:
                         break
                 
-                if nome != novoNome and idade != novaIdade:
-                    print("Cliente não encontrado! Tente novamente!")
-                    continue
                 else:
-                    break
-    
+                    print("Por favor, digite uma opção válida!")
+                    continue
+        except ValueError:
+            print("Por favor, digite um valor válido!")
+            self.atualizar_cliente()
+
     # Função de atualização dos dados de um produto
     def atualizar_produtos(self):
-        time.sleep(0.7)
-        print("\n[1] - Alterar somente descrição do produto\n[2] - Alterar somente o valor unitário do produto\n[3] - Alterar somente a categoria do produto\n[4] - Alterar todos os dados do produto")
-        escolha = int(input("Escolha uma das opções acima: "))
         try:
             while True:
+                time.sleep(0.7)
+                print("\n[1] - Alterar somente descrição do produto\n[2] - Alterar somente o valor unitário do produto\n[3] - Alterar somente a categoria do produto\n[4] - Alterar todos os dados do produto")
+                escolha = int(input("Escolha uma das opções acima: "))
+        
                 if escolha == 1:
                     time.sleep(0.7)
                     cursor = self.conn.cursor()
@@ -380,6 +379,7 @@ class Gerente(ConectarBanco):
                         break
                 else:
                     print("Por favor, selecione uma opção válida!")
+                    continue
         except ValueError:
             print("Por favor, digite um valor válido!")
 
